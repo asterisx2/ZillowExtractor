4cc49a0e83c7d7f2dd11174fd219e2f527e72e96
Ashwin Singh <ashwinsingh8@gmail.com>
2018-01-03 14:35:48 +0530

    Now with option to save Addresses for which no result were found in a file

--- Code/Main.py
+++ Code/Main.py
@@ -5,9 +5,10 @@ import CSVHelper
 
 utils = Utils()
 apiWorker = ZillowApiWorker
-zpids = []
+zpids = set()
 results = []
 noGood= []
+comparableProperties = 5
 
 def encode(val):
     return urllib.parse.quote_plus(val)
@@ -32,19 +33,22 @@ def get_results(zpids, results):
 
 def get_zpids(zpids, addresses):
     for address in addresses:
-        r = ZillowApiWorker.getZpids(encode(address[0]), encode(address[1] + "IL"))
-        if not r:
+        retSet = ZillowApiWorker.getZpids(encode(address[0]), encode(address[1] + "IL"))
+        if not retSet:
             print("No results for " + address[3])
             noGood.append(address[3])
         else:
-           zpids += r
+            for ret in retSet:
+                zpids.add(ret)
+                retSetComp = ZillowApiWorker.getDeepComps(ret,comparableProperties)
+                for retComp in retSetComp:
+                    zpids.add(retComp)
 
 def save_results():
     string = ""
     for result in results:
         string += result
     f = open("export.xml", "w")
-    print(string)
     f.write(str(string))
     f.close()
 
@@ -55,11 +59,10 @@ def save_NoGood(noGood):
     f.close();
 
 
-
 getAddresses()
 get_zpids(zpids, addresses)
 save_NoGood(noGood)
-#get_results(zpids, results)
+get_results(zpids, results)
 save_results()
 #CSVHelper.saveToCsv("export.csv",results)
 
--- Code/ZillowApiWorker.py
+++ Code/ZillowApiWorker.py
@@ -5,6 +5,7 @@ from lxml import etree
 zillowId = "X1-ZWz18wdrv6sq2z_3bebs"
 linksUrl = "http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=" + zillowId + "&address="
 propertyUrl = "http://www.zillow.com/webservice/GetUpdatedPropertyDetails.htm?zws-id="+zillowId+"&zpid="
+deepCompsUrl = "http://www.zillow.com/webservice/GetDeepComps.htm?zws-id=" + zillowId
 
 def printNode(node):
     print(etree.tostring(node, pretty_print=True))
@@ -25,11 +26,32 @@ def getZpids(address, citystatezip):
     results = xmldoc.xpath("/SearchResults:searchresults/response/results/result", namespaces = {
         'SearchResults': 'http://www.zillow.com/static/xsd/SearchResults.xsd'
     })
-    links = []
+    zpids = []
     for result in results:
         zpid = result.xpath("zpid", namespaces = {
         'zpid': 'http://www.zillow.com/static/xsd/SearchResults.xsd'
     })[0]
         print("[API]Found Property ZPID: "+zpid.text)
-        links.append(zpid.text)
-    return links
+        zpids.append(zpid.text)
+    return zpids
+
+def getDeepComps(zpid, count):
+    url = deepCompsUrl + "&zpid="+zpid+"&count="+str(count)
+    file = urllib.urlopen(url)
+    data = file
+    xmldoc = etree.parse(data)
+    file.close()
+
+    comps = xmldoc.xpath("/Comps:comps/response/properties/comparables/comp", namespaces={
+        'Comps': 'http://www.zillow.com/static/xsd/Comps.xsd'
+    })
+    zpids = []
+    for comp in comps:
+        zpid = comp.xpath("zpid", namespaces={
+            'zpid': 'http://www.zillow.com/static/xsd/SearchResults.xsd'
+        })[0]
+        print("[API][COMPARABLE]Found Property ZPID: " + zpid.text)
+        zpids.append(zpid.text)
+    return zpids
+
+
